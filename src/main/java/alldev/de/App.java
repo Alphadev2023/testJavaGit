b39f91fcc383fbf212ca49dac93d9edbecb54258
Diallo <alphadev2023@gmail.com>
2024-10-16 11:44:12 +0100

    commit dans une autre branche

--- src/main/java/alldev/de/App.java
+++ src/main/java/alldev/de/App.java
@@ -1,6 +1,8 @@
 package alldev.de;
 
+/*Je modifie mon fichier pour git */
 public class App{
+    //------fichier modifier-------
     public static void main(String[] args){
         System.out.println("Hello Git");
     }
